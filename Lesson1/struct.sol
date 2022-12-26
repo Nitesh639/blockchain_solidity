3a980e087fd3086ac9dd881e2d7cd021aeb35f98
Nitesh639 <nkush9598@gmail.com>
2022-12-26 17:29:40 +0530

    struct and array

---
+++ Lesson1/struct.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7; 
+
+contract SimpleStorage{
+
+    uint256 public favorateNumber;
+    // People public person = People ({ favorateNumber:2, name: "Nitesh"});
+    People[] public people;
+    struct People {
+        uint256 favorateNumber;
+        string name;
+    }
+
+    function store(uint256 _favorateNumber) public{
+        favorateNumber = _favorateNumber;
+    }
+    
+    function addPerson(string memory _name, uint256 _favorateNumber) public{
+        people.push(People({favorateNumber: _favorateNumber, name:_name}));
+    }
+}
